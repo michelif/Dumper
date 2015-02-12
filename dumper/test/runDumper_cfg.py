a553d6eb7e34e86833cd441316302493ad6feabe
Paolo Meridiani <paolo.meridiani@cern.ch>
2015-02-12 16:24:30 +0100

    Ripuliamo

--- dumper/test/runDumper_cfg.py
+++ dumper/test/runDumper_cfg.py
@@ -10,18 +10,13 @@ process.load('Configuration.StandardSequences.Services_cff')
 process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
-process.load('SimGeneral.MixingModule.mix_POISSON_average_cfi')
 process.load('Configuration.Geometry.GeometryExtended2023SHCalNoTaperReco_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
-process.load('Configuration.StandardSequences.RawToDigi_cff')
-process.load('Configuration.StandardSequences.L1Reco_cff')
-process.load('Configuration.StandardSequences.Reconstruction_cff')
 process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
-#process.GlobalTag = GlobalTag(process.GlobalTag, 'PH2_1K_FB_V4::All', '')
 
-
-process.GlobalTag.globaltag = 'PH2_1K_FB_V4::All'
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, 'PH2_1K_FB_V4::All', '')
 
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1),
@@ -35,14 +30,14 @@ process.options = cms.untracked.PSet(
 process.source = cms.Source("PoolSource",
 #                            fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/relval/CMSSW_6_2_0_SLHC23_patch1/RelValQCD_Pt_80_120_14TeV/GEN-SIM-RECO/PH2_1K_FB_V6_UPG23SHNoTaper-v2/00000/3A4509FB-6E9F-E411-A11B-002590494C62.root')
 #                            fileNames = cms.untracked.vstring('root://xrootd.unl.edu//store/mc/TP2023SHCALDR/DYToEE_M-20_TuneZ2star_14TeV-pythia6-tauola/GEN-SIM-RECO/SHCALJan23_PU140BX25_PH2_1K_FB_V6-v1/00000/002CD53C-57A7-E411-BB32-002618943901.root')
-
-                            fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/m/micheli/shashlikPerformance_2/CMSSW_6_2_0_SLHC23_patch1/src/HLTTest/MyCandidates/test/0683A170-619F-E411-A723-002481E9458E.root')
+#                            fileNames = cms.untracked.vstring('file:/afs/cern.ch/work/m/micheli/shashlikPerformance_2/CMSSW_6_2_0_SLHC23_patch1/src/HLTTest/MyCandidates/test/0683A170-619F-E411-A723-002481E9458E.root')
+                            fileNames = cms.untracked.vstring('/store/relval/CMSSW_6_2_0_SLHC23_patch1/RelValZEE_14TeV/GEN-SIM-RECO/PH2_1K_FB_V6_UPG23SHNoTaper-v2/00000/44D00C34-6B9F-E411-A697-02163E00F518.root')
 #                            fileNames = cms.untracked.vstring('file:./ZEE.root')
                             )
 
-
-process.multi5x5BasicClustersCleaned.endcapHitCollection = cms.string('EcalRecHitsEK')
-
+process.load("RecoEcal.EgammaClusterProducers.multi5x5ClusteringSequence_cff")
+process.multi5x5BasicClustersCleaned.endcapHitCollection = cms.string("EcalRecHitsEK")
+process.multi5x5BasicClustersUncleaned.endcapHitCollection = cms.string("EcalRecHitsEK")
 
 process.dumper = cms.EDAnalyzer('dumper',
                                    OutputFileName = cms.string("outDumper.root"),
@@ -51,9 +46,4 @@ process.dumper = cms.EDAnalyzer('dumper',
                                    trgSelection = cms.vstring("HLT_Ele27WP85_Gsf_v1", "HLT_Ele20WP60_Ele8_Mass55_v1","HLT_Ele25WP60_SC4_Mass5_v1",)
                                 )
 
-
-
-
-#process.p = cms.Path(process.multi5x5BasicClustersCleaned*process.dumper)
-#process.p = cms.Path(process.multi5x5BasicClustersCleaned)
-process.p = cms.Path(process.dumper)
+process.p = cms.Path(process.multi5x5ClusteringSequence * process.dumper)
