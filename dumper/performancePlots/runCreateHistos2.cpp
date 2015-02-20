b40979d5a8d21778230cf182e4a0bf328365b3ee
michelif <francesco.micheli@cern.ch>
2015-02-20 19:14:06 +0100

    first commit

---
+++ dumper/performancePlots/runCreateHistos2.cpp
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <fstream>
+#include "createHistos.h"
+
+using namespace std;
+
+
+
+int main( int argc, char* argv[] ) {
+
+
+
+  if(argc<2 || argc>3) {
+    cout << "Usage:  ./tmp/runCreateHistos listfile outputfile\n"
+	 << "  listfile:    list of root files incusing protocol eg dcap:/// .....\n"
+	 << "  outputfile:  name of output root file  eg output.root\n"
+	 << endl;
+    exit(-1);
+  }
+
+
+  
+  // Input list
+  char listName[500];
+  sprintf(listName,argv[1]); 
+  
+  // Output filename (.root)  
+  TString OutputFileName(argv[2]);
+  
+  // Name of input tree objects in (.root) files 
+  char treeName[100] = "tree";
+
+  //creating  TChain
+  TChain *chain = new TChain(treeName);
+  char pName[500];
+  ifstream is(listName);
+  if(! is.good()) {
+    cout << "int main() >> ERROR : file " << listName << " not read" << endl;
+    is.close();
+    exit(-1);
+  }
+  cout << "Reading list : " << listName << " ......." << endl;
+  
+  while( is.getline(pName, 500, '\n') ) {
+    if (pName[0] == '#') continue;
+    chain->Add(pName); 
+  }
+  is.close();
+  
+
+  createHistos theHistos(chain);
+  
+
+  theHistos.setOutputFile(OutputFileName);
+  theHistos.defineCategories();
+  theHistos.bookHistos();
+  theHistos.Loop2();
+  theHistos.writeHistos();
+
+  return 0;
+}
