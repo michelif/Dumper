5f714eb8748605ab42e02bf4cbc3b843055a8c95
michelif <francesco.micheli@cern.ch>
2015-03-06 10:44:32 +0100

    adding creation of lists

--- dumper/performancePlots/makePlots.sh
+++ dumper/performancePlots/makePlots.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-for version in V05_b V06_b V06_2_b V07_b V08_b V06_c;do
-
+for version in V16 V17 V18;do
+cd lists
+./makeLists.sh $version
+cd -
 #./tmp/runCreateHistos2 lists/DYToLLRelVal_$version.list DYToLLRelVal_$version.root
 #./tmp/runCreateHistos2 lists/DYToLLRelVal_noPU_$version.list DYToLLRelVal_noPU_$version.root
 rm plots/h*
 ./tmp/plotHistos DYToLLRelVal_noPU_$version.root DYToLLRelVal_$version.root plots_DYToLLRelVal_$version.root
-cp plots/h* ~/www/plots/shashlikUpgrade/2015-03-01/$version/
+cp plots/h* ~/www/plots/shashlikUpgrade/2015-03-05/$version/
 
 done
