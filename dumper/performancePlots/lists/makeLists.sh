4b1651961ab0f3f386cd723da97dc845bb197a5d
michelif <francesco.micheli@cern.ch>
2015-03-12 14:29:41 +0100

    adding all datasets

--- dumper/performancePlots/lists/makeLists.sh
+++ dumper/performancePlots/lists/makeLists.sh
@@ -2,15 +2,16 @@
 
 version=$1
 
+for dataset in DYToLLRelVal HggRelVal DYToEE GluGluToHToGG; do
 
-
-eos=/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select
-
-pathnoPU="root://eoscms//eos/cms/store/caf/user/micheli/ShashlikUpgrade/"$version"/DYToLLRelVal/noPU/"
-pathPU="root://eoscms//eos/cms/store/caf/user/micheli/ShashlikUpgrade/"$version"/DYToLLRelVal/PU140/"
-
-
-echo $pathPU
-
-$eos ls /eos/cms/store/caf/user/micheli/ShashlikUpgrade/$version/DYToLLRelVal/noPU | awk -v pathnoPU="root://eoscms//eos/cms/store/caf/user/micheli/ShashlikUpgrade/"$version"/DYToLLRelVal/noPU/" '{print pathnoPU $1}' > DYToLLRelVal_noPU_$version.list
-$eos ls /eos/cms/store/caf/user/micheli/ShashlikUpgrade/$version/DYToLLRelVal/PU140 | awk -v pathPU="root://eoscms//eos/cms/store/caf/user/micheli/ShashlikUpgrade/"$version"/DYToLLRelVal/PU140/" '{print pathPU $1}' > DYToLLRelVal_$version.list
+    eos=/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select
+    
+    pathnoPU="root://eoscms//eos/cms/store/caf/user/micheli/ShashlikUpgrade/"$version"/"$dataset"/noPU/"
+    pathPU="root://eoscms//eos/cms/store/caf/user/micheli/ShashlikUpgrade/"$version"/"$dataset"/PU140/"
+    
+    
+    echo $pathPU
+    
+    $eos ls /eos/cms/store/caf/user/micheli/ShashlikUpgrade/$version/$dataset/noPU | awk -v pathnoPU="root://eoscms//eos/cms/store/caf/user/micheli/ShashlikUpgrade/"$version"/"$dataset"/noPU/" '{print pathnoPU $1}' > $dataset"_noPU_"$version.list
+    $eos ls /eos/cms/store/caf/user/micheli/ShashlikUpgrade/$version/$dataset/PU140 | awk -v pathPU="root://eoscms//eos/cms/store/caf/user/micheli/ShashlikUpgrade/"$version"/"$dataset"/PU140/" '{print pathPU $1}' > $dataset"_"$version.list
+done
