662a79a13326c55289dade7d199c0eeb26822b35
michelif <francesco.micheli@cern.ch>
2015-03-12 15:17:03 +0100

    improved makeplots

--- dumper/performancePlots/makePlots.sh
+++ dumper/performancePlots/makePlots.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-for version in V19;do
-cd lists
-./makeLists.sh $version
-cd -
-./tmp/runCreateHistos2 lists/DYToLLRelVal_$version.list DYToLLRelVal_$version.root
-./tmp/runCreateHistos2 lists/DYToLLRelVal_noPU_$version.list DYToLLRelVal_noPU_$version.root
-rm plots/h*
-./tmp/plotHistos DYToLLRelVal_noPU_$version.root DYToLLRelVal_$version.root plots_DYToLLRelVal_$version.root
-cp plots/h* ~/www/plots/shashlikUpgrade/2015-03-05/$version/
+
+
+for version in V20 V21;do
+    cd lists
+    ./makeLists.sh $version
+    cd -
+    dateDir=`date +"%Y-%m-%d"`
+    mkdir ~/www/plots/shashlikUpgrade/$dateDir/
+    cp ~/www/plots/shashlikUpgrade/index.php  ~/www/plots/shashlikUpgrade/$dateDir/
+
+for dataset in  HggRelVal DYToLLRelVal; do
+    if [ "$dataset" == "DYToLLRelVal" ]
+    then
+	./tmp/runCreateHistos2 lists/$dataset"_"$version.list $dataset"_"$version.root   
+	./tmp/runCreateHistos2 lists/$dataset"_noPU_"$version.list $dataset_noPU_$version.root
+    fi
+
+    if [ "$dataset" == "HggRelVal" ]    
+    then
+	./tmp/runCreateHistosPhotons lists/$dataset"_"$version.list $dataset"_"$version.root   
+	./tmp/runCreateHistosPhotons lists/$dataset"_noPU_"$version.list $dataset_noPU_$version.root
+    fi
+
+    rm plots/h*
+   ./tmp/plotHistos $dataset"_noPU_"$version.root $dataset"_"$version.root plots_$dataset"_"$version.root
+   ./tmp/plotHistos $dataset"_noPU_"$version.root $dataset"_"$version.root plots_$dataset"_"$version.root
+    mkdir ~/www/plots/shashlikUpgrade/$dateDir/plots_$dataset"_"$version
+    cp plots/h* ~/www/plots/shashlikUpgrade/$dateDir/$version/
+    cp ~/www/plots/shashlikUpgrade/index.php  ~/www/plots/shashlikUpgrade/$dateDir/$version/
+done
 
 done
