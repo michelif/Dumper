babed6527d63753c7f0ef2e0be0cab58e89c406b
michelif <francesco.micheli@cern.ch>
2015-03-13 14:49:37 +0100

    some spell fixes

--- dumper/performancePlots/makePlots.sh
+++ dumper/performancePlots/makePlots.sh
@@ -2,33 +2,36 @@
 
 
 
-for version in V20 V21;do
+for version in V21_b;do
     cd lists
     ./makeLists.sh $version
     cd -
     dateDir=`date +"%Y-%m-%d"`
     mkdir ~/www/plots/shashlikUpgrade/$dateDir/
+    mkdir ~/www/plots/shashlikUpgrade/$dateDir/$version/
     cp ~/www/plots/shashlikUpgrade/index.php  ~/www/plots/shashlikUpgrade/$dateDir/
 
 for dataset in  HggRelVal DYToLLRelVal; do
     if [ "$dataset" == "DYToLLRelVal" ]
     then
 	./tmp/runCreateHistos2 lists/$dataset"_"$version.list $dataset"_"$version.root   
-	./tmp/runCreateHistos2 lists/$dataset"_noPU_"$version.list $dataset_noPU_$version.root
+	./tmp/runCreateHistos2 lists/$dataset"_noPU_"$version.list $dataset"_noPU_"$version.root
+    echo "DY ok"
     fi
 
     if [ "$dataset" == "HggRelVal" ]    
     then
 	./tmp/runCreateHistosPhotons lists/$dataset"_"$version.list $dataset"_"$version.root   
-	./tmp/runCreateHistosPhotons lists/$dataset"_noPU_"$version.list $dataset_noPU_$version.root
+	./tmp/runCreateHistosPhotons lists/$dataset"_noPU_"$version.list $dataset"_noPU_"$version.root
+    echo "Hgg ok" 
     fi
 
     rm plots/h*
    ./tmp/plotHistos $dataset"_noPU_"$version.root $dataset"_"$version.root plots_$dataset"_"$version.root
-   ./tmp/plotHistos $dataset"_noPU_"$version.root $dataset"_"$version.root plots_$dataset"_"$version.root
-    mkdir ~/www/plots/shashlikUpgrade/$dateDir/plots_$dataset"_"$version
-    cp plots/h* ~/www/plots/shashlikUpgrade/$dateDir/$version/
-    cp ~/www/plots/shashlikUpgrade/index.php  ~/www/plots/shashlikUpgrade/$dateDir/$version/
+    mkdir ~/www/plots/shashlikUpgrade/$dateDir/$version/$dataset
+    cp plots/h* ~/www/plots/shashlikUpgrade/$dateDir/$version/$dataset/
+    cp ~/www/plots/shashlikUpgrade/index.php  ~/www/plots/shashlikUpgrade/$dateDir/$version/index.php
+    cp ~/www/plots/shashlikUpgrade/index.php  ~/www/plots/shashlikUpgrade/$dateDir/$version/$dataset/index.php
 done
 
 done
