15d37f6f11ab282479f86cfc149a3f9897a3fd47
michelif <francesco.micheli@cern.ch>
2015-03-17 13:01:55 +0100

    fixing list splitting

--- dumper/performancePlots/lists/splitLists.sh
+++ dumper/performancePlots/lists/splitLists.sh
@@ -2,5 +2,6 @@
 
 version=$1
 
-split -l 10 -d GluGluToHToGG_$version.list GluGluToHToGG_$version"_"
+split -a 1 -l 10 -d GluGluToHToGG_$version.list GluGluToHToGG_$version"_"
+split -a 1 -l 10 -d DYToEE_$version.list DYToEE_$version"_"
 split -l 10 -d DYToEE_$version.list DYToEE_$version"_"
